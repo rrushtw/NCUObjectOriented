9bd116a5c08aba59d3e4d5f81bb25c719e14e6de
Sung-Ai <rrushtw@gmail.com>
2022-06-16 10:21:35 +0800

    Homework GUI pseudo code.

---
+++ PseudoCode/src/Models/MaximizeButton.java
@@ -0,0 +1,15 @@
+package Models;
+
+public class MaximizeButton extends BaseButton {
+    public MaximizeButton(Position position) {
+        super(position, 10, 10);
+    }
+
+    public void Draw() {
+        // draw button
+    }
+
+    public void Activate() {
+        // Minimize the window
+    }
+}
---
+++ PseudoCode/src/Models/MinimizeButton.java
@@ -0,0 +1,15 @@
+package Models;
+
+public class MinimizeButton extends BaseButton {
+    public MinimizeButton(Position position) {
+        super(position, 10, 10);
+    }
+
+    public void Draw() {
+        // draw button
+    }
+
+    public void Activate() {
+        // Minimize the window
+    }
+}
